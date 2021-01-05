645587679f814b5de12c9a80f8084dca5eae3877
jackupup <1241444086@qq.com>
2021-01-05 22:41:54 +0800

    第二次提交

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @ClassName: Test
+ * @Author: Administrator
+ * @CreateTime: 2021/1/5 0005 22:40
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("6666");
+    }
+}
